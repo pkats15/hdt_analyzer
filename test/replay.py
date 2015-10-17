ff94bf7b85099b8195dfefac304baa1e80cc63f1
pkats15 <pkats13@gmail.com>
2015-10-17 17:27:00 +0300

    Formatting

--- test/replay.py
+++ test/replay.py
@@ -1,7 +1,9 @@
 import json
 from cardType import card_type
 from os import path
-#import hdt_analyzer as hdt
+
+
+# import hdt_analyzer as hdt
 class Replay():
     @staticmethod
     def read_json(replay_file):
@@ -9,6 +11,7 @@ class Replay():
         json_data = json.load(data)
         data.close()
         return json_data
+
     @staticmethod
     def get_player_id(keypoints):
         for kp in keypoints:
@@ -16,7 +19,7 @@ class Replay():
                 if d["IsPlayer"] and "PLAYER_ID" in d["Tags"].keys():
                     return d['Tags']['PLAYER_ID']
 
-    def __init__(self,replay_file):
+    def __init__(self, replay_file):
         self.keypoints = Replay.read_json(replay_file)
         self.player_id = Replay.get_player_id(self.keypoints)
 
@@ -25,9 +28,9 @@ class Replay():
         for kp in self.keypoints:
             for d in kp['Data']:
                 if "CONTROLLER" in d['Tags'].keys() and "CARDTYPE" in d['Tags'].keys():
-                    if d['Tags']['CONTROLLER'] == self.player_id and d['Tags']['CARDTYPE'] == card_type('HERO') and d['CardId']!= None:
+                    if d['Tags']['CONTROLLER'] == self.player_id and d['Tags']['CARDTYPE'] == card_type('HERO') and d[
+                        'CardId'] != None:
                         hero = d['CardId']
                         break
         if hero != None:
             print hero
-
