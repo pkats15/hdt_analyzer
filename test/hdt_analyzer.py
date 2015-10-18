f1d8b89279cdcbb2fc5a1ce8bebc45770d13f891
pkats15 <pkats13@gmail.com>
2015-10-18 14:51:49 +0300

    Fixed the mulligans and added Database class

--- test/hdt_analyzer.py
+++ test/hdt_analyzer.py
@@ -1,18 +1,29 @@
 from os import path
 from replay import Replay
 from pprint import pprint
+from database import DataBase
 
-#TODO Find about 2way import
-#TODO Bind ids to card names
-#TODO Check results with HDT
+# TODO Find about 2way import
+# TODO Bind ids to card names
+# TODO Check results with HDT
 
 if __name__ == '__main__':
-    #Config:
-    files_folder = path.join(path.dirname(path.abspath(__file__)),'..','files')
-    replay_file = path.join(files_folder,'replay.json')
+    # Config:
+    files_folder = path.join(path.dirname(path.abspath(__file__)), '..', 'files')
+    replay_file = path.join(files_folder, 'replay.json')
+    databse_file = path.join(files_folder, 'database.json')
     replay = None
 
-    #Run
+    # Run
+    database = DataBase(databse_file)
     replay = Replay(replay_file)
-    replay.print_hero()
-    pprint(replay.get_cards_kept_by_player())
+    print('Your Hero: ' + database.get_card_name(replay.get_player_hero()))
+    l = []
+    lz = []
+    kept, mulliganed = replay.get_cards_kept_by_player()
+    for i in kept:
+        l.append(database.get_card_name(i))
+    print('Kept: ' + ', '.join(l))
+    for i in mulliganed:
+        lz.append(database.get_card_name(i))
+    print('Mulliganed: ' + ', '.join(lz))
--- test/replay.py
+++ test/replay.py
@@ -1,4 +1,5 @@
 import json
+import time
 from cardType import card_type
 from keypointType import keypoint_type, keypointType
 from pprint import pprint
@@ -7,6 +8,7 @@ from os import path
 
 # import hdt_analyzer as hdt
 class Replay():
+    the_coin = 'GAME_005'
     @staticmethod
     def read_json(replay_file):
         data = open(replay_file)
@@ -25,7 +27,7 @@ class Replay():
         self.keypoints = Replay.read_json(replay_file)
         self.player_id = Replay.get_player_id(self.keypoints)
 
-    def print_hero(self):
+    def get_player_hero(self):
         hero = None
         for kp in self.keypoints:
             for d in kp['Data']:
@@ -33,9 +35,21 @@ class Replay():
                     if d['Tags']['CONTROLLER'] == self.player_id and d['Tags']['CARDTYPE'] == card_type('HERO') and d[
                         'CardId'] != None:
                         hero = d['CardId']
-                        break
-        if hero != None:
-            print hero
+                        return hero
+        return None
+
+    def get_last_choosing_draw(self):
+        kps = self.keypoints
+        last_draw = -1
+        for i in range(len(kps)):
+            if kps[i]['Type'] == keypoint_type('Draw'):
+                for d in kps[i]['Data']:
+                    if d['IsPlayer'] and 'MULLIGAN_STATE'not in d['Tags']:
+                        last_draw = i
+        if last_draw != -1:
+            return last_draw
+        else:
+            return None
 
     def get_last_intro_draw(self):
         kps = self.keypoints
@@ -45,18 +59,6 @@ class Replay():
                 last_mull = i
         if last_mull != -1:
             return last_mull + 1
-        else:
-            return None
-
-    def get_last_choosing_draw(self):
-        kps = self.keypoints
-        first_mull = -1
-        for i in range(len(kps)):
-            if kps[i]['Type'] == keypoint_type('Mulligan'):
-                first_mull = i
-                break
-        if first_mull != -1:
-            return first_mull - 1
         else:
             return None
 
@@ -70,9 +72,9 @@ class Replay():
                 target_id = kps[i]['Id']
                 for d in kps[i]['Data']:
                     if d['Id'] == target_id and 'CARDTYPE' in d['Tags'] and 'CONTROLLER' in d['Tags'] and (
-                                        d['Tags']['CONTROLLER'] == 2 and d['Tags'][
+                                        d['Tags']['CONTROLLER'] == self.player_id and (d['Tags'][
                                     'CARDTYPE'] == card_type('MINION') or d['Tags']['CARDTYPE'] == card_type(
-                                "ABILITY")):
+                                "INVALID")) and d['CardId'] != Replay.the_coin):
                         card = d['CardId']
                         break
                 if card != None:
@@ -83,15 +85,15 @@ class Replay():
                 target_id = kps[i]['Id']
                 for d in kps[i]['Data']:
                     if d['Id'] == target_id and 'CARDTYPE' in d['Tags'] and 'CONTROLLER' in d['Tags'] and (
-                                        d['Tags']['CONTROLLER'] == 2 and d['Tags'][
+                                        d['Tags']['CONTROLLER'] == self.player_id and (d['Tags'][
                                     'CARDTYPE'] == card_type('MINION') or d['Tags']['CARDTYPE'] == card_type(
-                                "ABILITY")):
+                                "INVALID"))):
                         card = d['CardId']
                         break
                 if card != None:
                     cards_mulliganed.append(card)
         cards_kept = [item for item in cards_drawn if item not in cards_mulliganed]
-        return cards_kept, cards_mulliganed
+        return cards_kept,cards_mulliganed
 
         '''for kp in self.keypoints:
             if kp['Type']==keypointType['Draw']:
